ee3311067baff882849e262dd5942518ae1e54cc
sivamdq <sivaganesh.mdq@gmail.com>
2022-08-17 17:22:42 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -20,46 +20,72 @@ define('FPDF_FONTPATH', 'font/');
 class myPDF extends FPDF
 {
     // Page header
-    function Header()
+    public function Header()
     {
-        $this->Cell(50,30,'',1); 
-        // //$this->Line(89,35,290,35); //To draw horizontal line
-		// //To insert logo in pdf
-        // $this->Image('apple.jpg',27,20,30);
-
-        // $this->SetFont('Times','',10);
-        // $this->SetTextColor(0,0,0); 
-        // //$this->Cell(0,10,'',0,1);  //to leave empty space     
+        $this->SetFillColor(28,86,168);
+        $this->SetDrawColor(0,0,0);
+        $this->Rect(0,0,75,300,"DF");
+        $this->Image('apple.jpg',20,20,30);
+        
+        $this->Cell(20,30,'',0,1); 
+        $this->SetFont('Times','',10);
+        $this->SetTextColor(0,0,0);  
 		$this->Ln(10);
+    }
+
+    function LeftColumn($db)
+    {   
+        $this->SetFont('Times','',10);
+        $this->SetTextColor(0,0,0); 
+
+        $x = $this->GetX();
+        $y = $this->GetY();
+
+        // $test = json_decode(file_get_contents("php://input", true));
+        // $userId = $test->userId;
+
+        $stmt = $db->query("SELECT u.username, u.email, u.mobile, u.address, a.programming, a.server_database, a.operating_system, a.certification, a.basic_knowledge FROM users u INNER JOIN academic a ON u.id = a.user_id WHERE u.id = '1' ");
+
+        while($data = $stmt->fetch(PDO::FETCH_OBJ))
+        {
+            $this->SetXY($x -7, $y -25);
+            $this->Cell(18,6,'NAME ',0,0,'L');
+            $this->SetXY($x +11, $y -25); 
+            $this->MultiCell(53,6,': ' . $data->username,0,1); 
+
+            $this->SetXY($x -7, $y -18);              
+            $this->Cell(18,6,'EMAIL',0,0,'L');	
+            $this->SetXY($x +11, $y -18);
+            $this->MultiCell(53,6,': ' . $data->email,0,1);
+
+            $this->SetXY($x -7, $y -11);	
+            $this->Cell(18,6,'MOBILE',0,0,'L');
+            $this->SetXY($x +11, $y -11);
+            $this->Cell(53,6,': ' . $data->mobile,0,1);
+               
+            $this->SetXY($x -7, $y -4);
+            $this->Cell(18,6,'ADDRESS',0,0,'L');
+            $this->SetXY($x +11, $y -4);
+            $this->MultiCell(53,6,': ' . $data->address,0,1); 
+
+            //$this->Line(9,115,65,115); //To draw horizontal line
+
+            $this->SetXY($x +20, $y +30);
+            $this->SetFont('Times','B',12);
+            $this->Cell(18,6,'SKILLS',0,0);
+            // $this->SetXY($x +11, $y +3);
+            // $this->MultiCell(53,6,)
+            $this->Ln();
+        }
     }
 }
 
 /* Instanciation of inherited class */
 $pdf = new myPDF();
-$pdf->AliasNbPages();
-$pdf->AddPage('L', 'A4', 0);
-//$pdf->SetMargins(3, 44, 11.7);
+$pdf->AddPage();
+
 $pdf->Header();
-
-//$pdf->LeftColumn($db);
-
-// $pdf->User($db);
-// $pdf->AcademicAndProject($db);
+$pdf->LeftColumn($db);
 $pdf->Output();
-
-// $out = 'upload/inspection-' . microtime(true) . '.pdf';
-// $gen = $pdf->Output($out, 'F');
-
-// $link = "http://localhost/pdf/fpdf/";
-
-// $test = json_decode(file_get_contents("php://input", true));
-// $userId = $test->userId;
-
-// // $query = $db->query("SELECT username FROM users WHERE id = '".$userId."'");
-
-// //$file_name = str_replace($link, $query, $link.$out);
-
-// $stmt = $db->query("UPDATE users SET pdf_file = '". $link.$out ."' WHERE id = '".$userId."'");
-// echo json_encode(['message' => 'Link found', 'status' => 0, 'link' => $link.$out]);
 
 ?>
